9cf03c2dafa3fda175b83fc05fc0936d270c826f
Muhammad Anis <anisiconic@gmail.com>
2017-06-12 17:17:59 +0700

    Add room

--- django_project/channel_api/consumers.py
+++ django_project/channel_api/consumers.py
@@ -1,22 +1,29 @@
 from channels import Channel, Group
 from channels.sessions import channel_session
-from channels.auth import channel_session_user, channel_session_user_from_http
+from channels.auth import channel_session_user
 
 
 # Connected to websocket.connect
+@channel_session
 def ws_add(message):
     # Accept the connection
     message.reply_channel.send({"accept": True})
+    # get group name
+    room = message.content['path'].strip("/")
+    # set room in session
+    message.channel_session['room'] = room;
     # Add to the group
-    Group("hello-world").add(message.reply_channel)
+    Group(room).add(message.reply_channel)
 
 
+@channel_session
 def ws_message(message):
-    Group("hello-world").send({
+    Group(message.channel_session['room']).send({
         "text": message['text'],
     })
 
+
 # Connected to websocket.disconnect
-@channel_session_user
+@channel_session
 def ws_disconnect(message):
-    Group("hello-world").discard(message.reply_channel)
+    Group(message.channel_session['room']).discard(message.reply_channel)
