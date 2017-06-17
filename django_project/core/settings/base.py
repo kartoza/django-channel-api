cec1258a9f46ff874ab213e85892866a24c9a75b
Muhammad Anis <anisiconic@gmail.com>
2017-06-17 12:20:43 +0700

    Minified

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -8,8 +8,7 @@ from __future__ import absolute_import, unicode_literals
 from .utils import absolute_path
 
 ADMINS = (
-    ('Tim Sutton', 'tim@kartoza.com'),
-    ('Christian Christelis', 'christian@kartoza.com')
+    ('Muhammad Anis', 'anis@kartoza.com')
 )
 
 MANAGERS = ADMINS
@@ -113,9 +112,6 @@ TEMPLATES = [
                 "django.core.context_processors.media",
                 "django.core.context_processors.request",
                 "django.core.context_processors.tz",
-                # Dirty hack for now - should be specified in contrib.py
-                "mezzanine.conf.context_processors.settings",
-                "mezzanine.pages.context_processors.page",
             ],
         },
     },
